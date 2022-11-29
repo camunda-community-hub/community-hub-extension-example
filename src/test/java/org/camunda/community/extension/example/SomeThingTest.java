a379a28e1b3fd707240747048794481f7d2c6679
Simon Zambrovski <simon.zambrovski@holisticon.de>
2022-11-29 21:42:30 +0100

    add manual failing test and GH trigger

--- src/test/java/org/camunda/community/extension/example/SomeThingTest.java
+++ src/test/java/org/camunda/community/extension/example/SomeThingTest.java
@@ -1,15 +1,25 @@
 package org.camunda.community.extension.example;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class SomeThingTest {
 
-    private SomeThing testThing = new SomeThing();
+    private final SomeThing testThing = new SomeThing();
 
     @Test
     public void testDoIt() {
         assertThat(testThing.doIt(1)).isEqualTo(2);
+    }
+
+    /**
+     * Tests fails if called with -DfailTest=true
+     */
+    @Test
+    public void testToFail() {
+        final Boolean failTest = Boolean.valueOf(System.getProperty("failTest", "false"));
+        assertThat(failTest).isEqualTo(false);
     }
 }
